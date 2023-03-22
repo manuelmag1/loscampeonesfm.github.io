b7d61efb45be1969faedaf7c46055193dd1ea19b
Manuel Maglioni <maglioni124@gmail.com>
2023-03-22 16:22:40 +0100

    tarde

--- js/script.js
+++ js/script.js
@@ -69,28 +69,28 @@
 
   // HORA SEMANA
 
-  // function updateLiveText() {
-  //   const now = new Date().toLocaleString("es-CO", {
-  //     timeZone: "America/Bogota",
-  //   });
-  //   const [date, time] = now.split(", ");
+  function updateLiveText() {
+    const now = new Date().toLocaleString("es-CO", {
+      timeZone: "America/Bogota",
+    });
+    const [date, time] = now.split(", ");
 
-  //   const startHour = "13:00";
-  //   const endHour = "15:00";
+    const startHour = "10:00";
+    const endHour = "12:30";
 
-  //   if (time >= startHour && time <= endHour) {
-  //     document.getElementById("live").innerHTML = "EN VIVO";
-  //     const vsImage = document.getElementById("vs");
-  //     vsImage.style.display = "none";
-  //   } else {
-  //     document.getElementById("live").innerHTML =
-  //       "LUNES A VIERNES <br> 1:00 PM - 3:00 PM";
-  //     const vsImage = document.getElementById("vs");
-  //     vsImage.style.display = "none";
-  //   }
-  // }
+    if (time >= startHour && time <= endHour) {
+      document.getElementById("live").innerHTML = "EN VIVO";
+      const vsImage = document.getElementById("vs");
+      vsImage.style.display = "block";
+    } else {
+      document.getElementById("live").innerHTML =
+        "LUNES A VIERNES <br> 1:00 PM - 3:00 PM";
+      const vsImage = document.getElementById("vs");
+      vsImage.style.display = "none";
+    }
+  }
 
-  // setInterval(updateLiveText, 1000);
+  setInterval(updateLiveText, 1000);
 
   // let intervalId = null;
   // function updateLiveStatus() {
