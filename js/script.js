0868e8d90d3037bb1398f3c01ac53223365da5ee
Manuel Maglioni <maglioni124@gmail.com>
2023-03-22 19:04:14 +0100

    1pm

--- js/script.js
+++ js/script.js
@@ -75,8 +75,8 @@
     });
     const [date, time] = now.split(", ");
 
-    const startHour = "13:00";
-    const endHour = "15:00";
+    const startHour = "1:00";
+    const endHour = "3:00";
 
     if (time >= startHour && time <= endHour) {
       document.getElementById("live").innerHTML = "EN VIVO";
