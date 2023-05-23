b885dfb9771b5e341e66f43e1b6fd1d1878335e3
manuelmag1 <maglioni124@hotmail.com>
2023-05-23 20:42:50 +0200

    pc

--- js/script.js
+++ js/script.js
@@ -69,28 +69,6 @@
 
   // HORA SEMANA
 
-  // function updateLiveText() {
-  //   const now = new Date().toLocaleString("es-CO", {
-  //     timeZone: "America/Bogota",
-  //   });
-  //   const [date, time] = now.split(", ");
-
-  //   const startHour = "5:00";
-  //   const endHour = "8:00";
-
-  //   if (time >= startHour && time <= endHour) {
-  //     document.getElementById("live").innerHTML = "EN VIVO";
-  //     const vsImage = document.getElementById("vs");
-  //     vsImage.style.display = "none";
-  //   } else {
-  //     document.getElementById("live").innerHTML =
-  //       "LUNES A VIERNES <br> 1:00 PM - 3:00 PM";
-  //     const vsImage = document.getElementById("vs");
-  //     vsImage.style.display = "none";
-  //   }
-  // }
-
-//  HORA DE PARTIDO
   function updateLiveText() {
     const now = new Date().toLocaleString("es-CO", {
       timeZone: "America/Bogota",
@@ -98,19 +76,41 @@
     const [date, time] = now.split(", ");
 
     const startHour = "5:00";
-    const endHour = "9:00";
+    const endHour = "8:00";
 
     if (time >= startHour && time <= endHour) {
       document.getElementById("live").innerHTML = "EN VIVO";
       const vsImage = document.getElementById("vs");
-      vsImage.style.display = "block";
+      vsImage.style.display = "none";
     } else {
       document.getElementById("live").innerHTML =
-        "5:00 PM";
+        "LUNES A VIERNES <br> 1:00 PM - 3:00 PM";
       const vsImage = document.getElementById("vs");
-      vsImage.style.display = "block";
+      vsImage.style.display = "none";
     }
   }
+
+//  HORA DE PARTIDO
+  // function updateLiveText() {
+  //   const now = new Date().toLocaleString("es-CO", {
+  //     timeZone: "America/Bogota",
+  //   });
+  //   const [date, time] = now.split(", ");
+
+  //   const startHour = "5:00";
+  //   const endHour = "9:00";
+
+  //   if (time >= startHour && time <= endHour) {
+  //     document.getElementById("live").innerHTML = "EN VIVO";
+  //     const vsImage = document.getElementById("vs");
+  //     vsImage.style.display = "block";
+  //   } else {
+  //     document.getElementById("live").innerHTML =
+  //       "5:00 PM";
+  //     const vsImage = document.getElementById("vs");
+  //     vsImage.style.display = "block";
+  //   }
+  // }
 
   setInterval(updateLiveText, 1000);
 
